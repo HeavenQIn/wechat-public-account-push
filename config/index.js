5e88f9beee0d2051b3871086677ec2a5f152f4f2
HeavenQIn <113609704+HeavenQIn@users.noreply.github.com>
2022-09-15 22:59:02 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -103,7 +103,7 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "",
+  CALLBACK_TEMPLATE_ID: "--v5NN02uHswOM7HV3a6XgfKpjlGKJTJmnYSINAdqTA",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -111,7 +111,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oJDRn653jn6EJxDe0XrUEkUeC2G4",
     },
     // 你可以不断按格式往下增加
     // ...
